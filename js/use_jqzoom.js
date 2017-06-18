2b0e221bcd5810299149acb38cd381e62d3646a9
wufff <wfqjnzb@sina.com>
2017-06-18 10:56:45 +0800

    01

---
+++ js/use_jqzoom.js
@@ -0,0 +1,14 @@
+﻿/*使用jqzoom*/
+$(function(){
+	$('.jqzoom').jqzoom({
+		zoomType: 'standard',
+		lens:true,
+		preloadImages: false,
+		alwaysOn:false,
+		zoomWidth: 380,
+		zoomHeight: 380,
+		xOffset:10,
+		yOffset:0,
+		position:'right'
+    });
+});
